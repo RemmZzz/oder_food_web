3d1724e0ab7d7c1db6c53dbe347ecbcaf3a24179
MinhBui <buivanremdh@gmail.com>
2025-09-07 19:05:02 +0700

    Add backend project

---
+++ oder_food_be/src/main/java/org/example/oder_food_be/OderFoodBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.oder_food_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OderFoodBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OderFoodBeApplication.class, args);
+	}
+
+}
---
+++ oder_food_be/src/test/java/org/example/oder_food_be/OderFoodBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oder_food_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OderFoodBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
